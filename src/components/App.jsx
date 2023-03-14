922601585bfcf57e82dc6e361fcf189cce087ec9
VladyslavLiapko <109310520+VladyslavLiapko@users.noreply.github.com>
2023-03-14 21:09:17 +0200

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,46 +7,52 @@ import Notification from './Notification/Notification.jsx';
 
 
 
-const App = () => {
+export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const countPositiveFeedbackPercentage = () => {
-    return Math.round(good / (totalCount() / 100));
-  };
-  
-  const onLeaveFeedback = event => {
-    switch (event.target.dataset.action) {
-      case 'good':
-        setGood(state => state + 1);
-        break;
-      case 'neutral':
-        setNeutral(state => state + 1);
-        break;
-      case 'bad':
-        setBad(state => state + 1);
-        break;
-      default:
-        break;
-    }
-  }
-
-
   const totalCount = () => {
     return good + bad + neutral;
   };
+
+  const countPositiveFeedbackPercentage = () => {
+    let totalFeedback = totalCount();
+    return totalFeedback ? Math.round((good / totalFeedback) * 100) : 0;
+    
+  };
   
- 
+  const handleFeedback = ({ target }) => {
+    const { feedback } = target.dataset;
+
+    switch (feedback) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+      
+      default:
+        return;
+    }
+  };
+
   return (
     <>
       <Section title={'Please leave feedback'}>
         <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={onLeaveFeedback}
+           options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
         />
+      </Section>
+      <Section title={'Statistics'}>
         {totalCount() > 0 ? (
-      
           <Statistics
             good={good}
             neutral={neutral}
@@ -59,9 +65,6 @@ const App = () => {
         )}
       </Section>
     </>
-      )
-  };
+  );
     
-  
-export default App;
- 
+}
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 
-export default function Notification({ message }) {
+const  Notification = ({ message }) => {
   return <p>{message}</p>;
 }
+export default Notification;
 
 Notification.propTypes = {
-  message: PropTypes.string.isRequired,
+  message: PropTypes.string,
 };
